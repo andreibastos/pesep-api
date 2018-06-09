7a29a86189071ee0170b153ecaba337fa9a729d7
Andrei Bastos <andreibastos@outlook.com>
2018-06-09 15:40:55 -0300

    headers do FPO
    cabeçário para saber quais colunas existem

--- src/app.py
+++ src/app.py
@@ -18,40 +18,29 @@ import os
 from models.power_flow import PowerFlow
 
 ######################### Configurações ##################################
-PESEP_CORE_FOLDER = os.environ.get('PESEP_CORE_FOLDER', '~/PESEP/core/')
 app = Flask(__name__)
 app.debug = True
 app.threaded = True
-app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * \
-    1024  # limitação de 5 MB de arquivo de upload
 app.config['SECRET_KEY'] = 'super-secret'
+
 
 # Caminhos das rotas
 route_default = "/"
 route_default_calcules = route_default + "calcule"
-route_default_user = route_default + "user"
-ALLOWED_EXTENSIONS = set(['txt', 'csv', 'json', 'png', 'jpg', 'jpeg'])
-
+route_default_config = route_default + "config"
 cors = CORS(app, resources={route_default_calcules+"/*": {"origins": "*"}})
 
+
+
 ####################### Funções comuns #######################################
-
-
 def datetime_to_timestamp(dt):
     timestamp = (dt - datetime.datetime(1970, 1, 1)).total_seconds()
     return int(timestamp)
 
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 ####################### Classes #############################################
-
 
 class InvalidUsage(Exception):
     status_code = 400  # codigo padrão de erro
-
     def __init__(self, message_error, status_code=None, payload=None):
         Exception.__init__(self)
         self.message_error = message_error
@@ -68,14 +57,11 @@ class InvalidUsage(Exception):
         return rv
 
 ######################## Erros ###############################################
-
-
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
-
 
 ######################## Rotas ###############################################
 ########### cálculos #######################################################
@@ -99,10 +85,7 @@ def power_flow():
     except Exception as identifier:
         raise InvalidUsage(identifier.message)
 
-
 # Curto Circuito
-
-
 @app.route(route_default_calcules+"/short", methods=['POST'])
 def short_circuit():
     if request.method == 'POST':
@@ -113,10 +96,8 @@ def short_circuit():
         else:
             return json.dumps({"error": request.headers['Content-Type']})
 
-
 def verify_consistency(lines, buses):
     return True
-
 
 def calcule_power_flow(lines, buses):
     try:
@@ -130,6 +111,17 @@ def calcule_power_flow(lines, buses):
         raise
 
 
+########## Configurações
+@app.route(route_default_config + "/headers")
+def get_headers():
+    f = open('./config/headers_files.json', 'r')
+    HEADERS = json.load(f)
+    resp = Response(json.dumps(HEADERS), status=200,
+                    mimetype='application/json')
+    f.close()
+    return resp 
+
 ######################## Função Principal ######################################    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=os.environ.get('port', 5000))
+    
