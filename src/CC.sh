005b8d5fad7aa0eff6a959f0c93e907ffaa1fb23
Andrei Bastos <andreibastos@outlook.com>
2018-07-02 17:45:10 -0300

    comentários nos arquivos shell

--- src/CC.sh
+++ src/CC.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-dir=$1;
 
-./FPO.sh $dir
+# recebe a pasta que deve ser criada como pârametro
+dir=$1; 
 
+# cria a pasta com o parâmetro recebido
 mkdir -p $dir
 
+# calcula o fluxo de potência para obter a matriz de susceptancia 
+./FPO.sh $dir
+
+# copia o compilado do CC para a nova pasta
 cp ../dist/* $dir/
 
+# navega até a pasta
 cd $dir 
 
+# executa o curto e imprime o log no arquivo de log_CC.txt
 ./CC >> log_CC.txt
 
+# remove o arquivo compilado
 rm CC
