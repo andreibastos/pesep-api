db6fe1554006d9c8d347b360fa75a9f2cc67df2e
Andrei Bastos <andreibastos.developer@gmail.com>
2019-05-18 21:33:57 -0300

    Alteração de Variaveis de Ambiente

--- src/settings.py
+++ src/settings.py
@@ -8,3 +8,5 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 ROOT_DIR = os.getenv('ROOT_DIR')
 SRC_DIR = ROOT_DIR + os.getenv('SRC_DIR')
 HEADER_FILES = ROOT_DIR + os.getenv('HEADER_FILES')
+PORT = os.environ.get('PORT', 5000)
+DEBUG = os.environ.get('DEBUG', False) == 'True'
